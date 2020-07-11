04d818da9c3f311ad55b7dcaa725c87598a591e5
rb19 <raymon.baek@gmail.com>
2020-07-11 19:32:09 -0400

    consolidate: add consolidate tool

---
+++ consolidate.sh
@@ -0,0 +1,28 @@
+# Enter the path where the bulkdownloader script lives."
+script_path="./bulk-downloader-for-reddit/script.py"
+# Enter the full path of where you want the files to end up.
+target_path="/path/to/target"
+# Where the files will go (should match first script prompt)
+downloads_path="/path/to/downloads"
+
+python3 $script_path
+
+# Having LOG_FILES breaks the push/pop stack so this is deleted.
+rm -rf LOG_FILES
+dirlvl=0;
+for d in */;
+do 
+    # Checks if there's any subdirectories in the current directory.
+    ls -d */ >/dev/null 2>&1 ;
+    if [ $? == 0 ]; 
+    then 
+        pushd $d
+        ((dirlvl++))
+        rsync -arPv ./*.{jpg,png} $target_path/.
+        echo $dirlvl
+    else
+        dirlvl=0;
+        echo $dirlvl
+        popd
+    fi
+done
